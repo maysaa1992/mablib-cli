c4c9542b74f21b4ed41c39281050bda9a564b453
maysaa1992 <batainehmaysaa@gmail.com>
2023-05-10 13:37:20 +0000

    second sub

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,13 +1,12 @@
 import re
-mylist = []
-text_list=[]
-list_join=""
-input_list=[]
+
+def intro ():
+   print ('welcom to madlib game')
+
+
 def read_template (path):
     try:
-      print ('welcom to madlib game')
-
-      with open(path) as f:
+      with open(path ,'r') as f:
        contant =f.read()
        print(contant)
        return contant    
@@ -15,49 +14,48 @@ def read_template (path):
         raise FileNotFoundError("no such file")
       
 def parse_template(text):
-  
-  text_list=text.split()
-  rgx= ('{/[a-zA-Z]}')
-  for i in range(len(text_list)): 
-   # match = re.search('{/[a-zA-Z]}', text_list[i])
-    if text_list[i]=="{Adjective}":
-      mylist.append(text_list[i])
-    #re.sub("{Adjective}", "{}", text_list[i])
-      text_list[i]="{}"
-  list_join=' '.join(text_list)
-  print (mylist)
-  print(list_join)
-  return list_join,mylist
+  words_list = re.findall(r'{(\w+)}', text)
+  clean_text = re.sub(r'{\w+}', '{}', text)
+  return clean_text,tuple(words_list)
 
 def input_fun(mylist):
-  for i in range(len(mylist)): 
+  input_list=[]
+  for i in range(0,len(mylist)): 
     print("enter",mylist[i]) 
     val=  input(">") 
     input_list.append(val)
   print(input_list)
   return input_list 
           
-def merge(text,list):
-  print (text)
- # print (list)
-  text_split=text.split()
-  print (text_split)
-  index=0
-  for i in range(len(text_split)):
-    if text_split[i]=='{}':
-      print(list[index])
-      text_split[i]=list[index]
-      index= index+1
-  text_join=' '.join(text_split)
-  print(text_join)
-  return text_join
+def merge(text,parts):
+  text=text.format(*parts)
+  return text
+#   print (text)
+#  # print (list)
+#   text_split=text.split()
+#   print (text_split)
+#   index=0
+#   for i in range(len(text_split)):
+#     if text_split[i]=='{}':
+#       print(list[index])
+#       text_split[i]=list[index]
+#       index= index+1
+#   text_join=' '.join(text_split)
+#   print(text_join)
+#   return text_join
+def new_file(merget_text):
+  with open("../assets/new_file.txt","w") as f:
+   f.write(merget_text)
+  
     
-         
-
-text=read_template("../assets/dark_and_stormy_night_template.txt")
-stripped ,parts =parse_template(text)
-input_val=input_fun(parts)
-merge(stripped,input_val)   
+if  __name__ == "__main__":        
+ intro()
+ text=read_template("../assets/dark_and_stormy_night_template.txt")
+ stripped,parts=parse_template(text)
+ input_val=input_fun(parts)
+ merget_text =merge(stripped,input_val)
+ print(merget_text)
+ new_file(merget_text)   
 
 
 
